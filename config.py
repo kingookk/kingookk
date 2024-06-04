3f91ad5cb2dc3275f97d55a9faf3b424525e3eee
kingookk <138496905+kingookk@users.noreply.github.com>
2024-06-04 11:36:34 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/LeoTeto/RX79R",
+    "https://github.com/kingookk/kingookk",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
