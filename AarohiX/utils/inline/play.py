a683ed0d96ded3dcaa25beb260484997e933190b
kingookk <138496905+kingookk@users.noreply.github.com>
2024-06-09 08:26:16 +0300

    Add files via upload

--- AarohiX/utils/inline/play.py
+++ AarohiX/utils/inline/play.py
@@ -1,3 +1,4 @@
+
 import math
 from config import SUPPORT_CHAT, OWNER_ID
 from pyrogram.types import InlineKeyboardButton
@@ -19,10 +20,10 @@ def track_markup(_, videoid, user_id, channel, fplay):
         ],
         [
             InlineKeyboardButton(
-                text="𝗗𝗲𝘃", url=f"tg://openmessage?user_id={OWNER_ID}",
+                text="💝ᴍᴀɪɴᴛᴀɪɴᴇʀ[ᴅɪʟ]💝", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="𝗦𝗼𝗨𝗿𝗰𝗲", url=SUPPORT_CHAT,
+                text="🥰sᴜᴩᴩᴏʀᴛ[ᴀɪᴍ]🥰", url=SUPPORT_CHAT,
             )
         ],
         [
@@ -41,25 +42,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "⊚━━━━━━━━━"
+        bar = "♡╌╌╌╌╌╌╌╌"
     elif 10 < umm < 20:
-        bar = "━⊚━━━━━━━━"
+        bar = "╌♡╌╌╌╌╌╌╌"
     elif 20 <= umm < 30:
-        bar = "━━⊚━━━━━━━"
+        bar = "╌╌♡╌╌╌╌╌╌"
     elif 30 <= umm < 40:
-        bar = "━━━⊚━━━━━━"
+        bar = "╌╌╌♡╌╌╌╌╌"
     elif 40 <= umm < 50:
-        bar = "━━━━⊚━━━━━"
+        bar = "╌╌╌╌♡╌╌╌╌"
     elif 50 <= umm < 60:
-        bar = "━━━━━⊚━━━━"
+        bar = "╌╌╌╌╌♡╌╌╌"
     elif 60 <= umm < 70:
-        bar = "━━━━━━⊚━━━"
+        bar = "╌╌╌╌╌╌♡╌╌"
     elif 70 <= umm < 80:
-        bar = "━━━━━━━⊚━━"
-    elif 80 <= umm < 90:
-        bar = "━━━━━━━━⊚━"
+        bar = "╌╌╌╌╌╌╌♡╌"
+    elif 80 <= umm < 95:
+        bar = "╌╌╌╌╌╌╌╌♡╌"
     else:
-        bar = "━━━━━━━━━⊚"
+        bar = "╌╌╌╌╌╌╌╌╌♡"
     buttons = [
         [
             InlineKeyboardButton(
@@ -70,14 +71,13 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="𝗥𝗲𝗽𝗟𝗮𝘆", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="🥰ʀᴇᴘʟᴀʏ[↻]🥰", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(
-                text="𝗦𝗼𝗨𝗿𝗰𝗲", url=SUPPORT_CHAT,
+                text="🥰sᴜᴩᴩᴏʀᴛ[ᴀɪᴍ]🥰", url=SUPPORT_CHAT,
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -96,10 +96,10 @@ def stream_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="𝗗𝗲𝘃", url=f"tg://openmessage?user_id={OWNER_ID}",
+                text="💝ᴍᴀɪɴᴛᴀɪɴᴇʀ[ᴅɪʟ]💝", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="𝗦𝗼𝗨𝗿𝗰𝗲", url=SUPPORT_CHAT,
+                text="🥰sᴜᴩᴩᴏʀᴛ[ᴀɪᴍ]🥰", url=SUPPORT_CHAT,
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
